34a3e7d9ae4bb12a1ab54d727d3b68ff90f8335f
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2019-12-26 13:51:13 +0300

    remove not necessary
    remove not necessary

--- 3.js
+++ 3.js
@@ -2,9 +2,6 @@ const textWindow = document.querySelector("input[type=text");
 const clickBtn = document.getElementById("clickBtn");
 const bodyDiv = document.querySelector("div.bodyDiv");
 
-clickBtn.par
-
-
 function countDown() {
     let repeatNumber = Number(textWindow.value);
     for(let i=1; i<=repeatNumber; i++){
