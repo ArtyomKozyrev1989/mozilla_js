38d34125b073f68fe6af5021a815c992bde4159e
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-01-01 18:47:43 +0300

    json example

--- json_tr.js
+++ json_tr.js
@@ -1,10 +1,39 @@
-let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-let request = new XMLHttpRequest();
-request.open("GET", requestURL);
-request.responseType = "json";
-request.send();
+const upperUL = document.body.querySelector("ul"); // find ul element
 
-request.onload = function () {
-    let x = request.response;
-    console.log(x["members"][0]);
-};
+// create request here:
+
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let requestAPI = new XMLHttpRequest();
+requestAPI.responseType = "json";
+requestAPI.open("GET", requestURL);
+requestAPI.send();
+
+// get json here:
+
+requestAPI.onload = function () {
+    const members = requestAPI.response['members'];
+    for (let i=0; i<members.length; i++) {
+        let heroName = document.createElement('li');
+        heroName.innerText = members[i]['name'];
+        heroName.style.color = "red";
+        upperUL.appendChild(heroName);
+
+        let abilitiesArray = document.createElement('ul');
+        abilitiesArray.style.color = "black";
+        abilitiesArray.className = "abilityList"
+        heroName.appendChild(abilitiesArray);
+
+        for (let j=0; j<members[i]['powers'].length; j++) {
+            let ability = document.createElement('li');
+            ability.innerText = members[i]['powers'][j];
+            abilitiesArray.appendChild(ability);
+        };
+    };
+}
+
+
+
+
+
+
+
