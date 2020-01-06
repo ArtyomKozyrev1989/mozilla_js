c43ad950c36db784eff37510e89751a5a97acf5a
Artyom Kozyrev <43035134+ArtyomKozyrev1989@users.noreply.github.com>
2020-01-06 23:03:17 +0300

    timer

---
+++ timer.js
@@ -0,0 +1,37 @@
+const startBtn = document.querySelector("button#startBtn");
+const stopBtn = document.querySelector("button#stopBtn");
+const resetBtn = document.querySelector("button#resetBtn");
+const resultField = document.querySelector("p#result");
+let timerHandler = null;
+let secondsLeft = 0;
+let active = false;
+
+let startCount = function() {
+    if (!active) {
+        timerHandler = setInterval(() => {
+            secondsLeft += 1;
+            resultField.innerText = `Seconds left: ${secondsLeft}`;
+            active = true;
+        }, 1000);
+    }
+};
+
+let stopCount = function() {
+    if(active) {
+        clearInterval(timerHandler);
+        active = false;
+    }
+};
+
+let resetCounter = function() {
+    if(timerHandler) {
+        secondsLeft = 0;
+        resultField.innerText = `Seconds left: ${secondsLeft}`;
+        clearInterval(timerHandler);
+        active = false;
+    }
+};
+
+startBtn.addEventListener("click", startCount);
+stopBtn.addEventListener("click", stopCount);
+resetBtn.addEventListener("click", resetCounter);
